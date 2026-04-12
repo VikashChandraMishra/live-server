c380a578698fd5d94dc25eb0eb83f45848406b29
Vikash Chandra Mishra <crimsonsyrus000@gmail.com>
2026-04-12 20:53:25 +0530

    add logs for server listening and error

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ const server = http.createServer((_req, res) => {
     res.end('Hello World!');
 });
 
-server.listen(port);
+server.listen(port, () => {
+    console.log("Server listening on port ", port);
+});
+
+server.on('error', (err) => {
+    console.error('Server error:', err.message);
+});
